0ee99f5f16fe9ebcf3cc90f75fa55e3c0b97b0df
Michael Bradvica <mjbradvica@gmail.com>
2024-05-23 19:19:29 -0400

    test fix

--- src/library/hooks/UseInput.ts
+++ src/library/hooks/UseInput.ts
@@ -5,12 +5,13 @@ export default function useInput<T>(
   defaultValue: T,
   conversionFunc: (newValue: string) => T,
 ): IUseInput<T> {
-  const [valid] = useState<boolean>(false);
+  const [valid, setValid] = useState<boolean>(false);
   const [value, setValue] = useState<T>(defaultValue);
 
   const updateValue = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const element = event.target as HTMLInputElement;
 
+    setValid(element.validity.valid);
     setValue(conversionFunc(element.value));
   };
 
