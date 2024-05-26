85d7d6d251521eb541aa9b9408c66744f71e2d5a
Michael B <mjbradvica@gmail.com>
2024-05-26 18:05:35 -0400

    test updates

--- src/library/base/input/EazyInput.test.tsx
+++ src/library/base/input/EazyInput.test.tsx
@@ -1,45 +1,50 @@
-import IUseInput from "../../interfaces/IUseInput";
+import { defaultTextInput } from "../../utilities/DefaultInputs";
 import { EazyInput } from "./EazyInput";
 import { render, screen } from "@testing-library/react";
 
 describe("baseInput", () => {
   it("has correct default values", () => {
-    const input: IUseInput<string> = {
-      value: "123",
-      valid: true,
-      updateValue: vi.fn(),
-      dirty: true,
-      error: "",
-    };
-
     const id = "123";
     const placeholder = "placeholder";
 
-    render(<EazyInput input={input} id={id} placeholder={placeholder} />);
+    render(
+      <EazyInput input={defaultTextInput} id={id} placeholder={placeholder} />,
+    );
 
     const element = screen.getByRole("textbox");
 
-    expect(element).toHaveValue(input.value);
+    expect(element).toHaveValue("");
     expect(element).toHaveAttribute("id", id);
     expect(element).not.toBeRequired();
     expect(element).toHaveAttribute("aria-labelledby", id);
   });
 
   it("has correct placeholder", () => {
-    const input: IUseInput<string> = {
-      value: "",
-      valid: true,
-      updateValue: vi.fn(),
-      dirty: true,
-      error: "",
-    };
-
     const id = "123";
     const placeholder = "placeholder";
 
-    render(<EazyInput input={input} id={id} placeholder={placeholder} />);
+    render(
+      <EazyInput input={defaultTextInput} id={id} placeholder={placeholder} />,
+    );
 
     const element = screen.getByPlaceholderText(placeholder);
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it("has correct title", () => {
+    const title = "title";
+
+    render(
+      <EazyInput
+        input={defaultTextInput}
+        id="123"
+        placeholder="placeholder"
+        title={title}
+      />,
+    );
+
+    const element = screen.getByTitle(title);
 
     expect(element).toBeInTheDocument();
   });
